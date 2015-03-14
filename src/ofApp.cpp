f69dff2a4de421c2e516eb29cbe18cc485cea70e
gaborparsons <tankb969@newschool.edu>
2015-03-13 22:44:01 -0400

    First

---
+++ src/ofApp.cpp
@@ -0,0 +1,488 @@
+/**
+ *
+ * OFDevCon Example Code Sprint
+ * Camera Ribbon example
+ * This example generates ribbons along the mouse trail that descend in space
+ * When you click space bar, you can
+ *
+ * Created by James George for openFrameworks workshop at Waves Festival Vienna sponsored by Lichterloh and Pratersauna
+ * Adapted during ofDevCon on 2/23/2012
+ */
+
+
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    
+    //just set up the openFrameworks stuff
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofBackground(255);
+   
+    
+    //initialize the variable so it's off at the beginning
+    usecamera = false;
+    
+    
+    //**********************************************
+    //**********************************************
+    //setup the audio input
+    
+    soundStream.listDevices();
+    
+    int bufferSize = 256;
+    
+    left.assign(bufferSize, 0.0);
+    
+    volHistory.assign(400, 0.0);
+    
+    bufferCounter	= 0;
+    drawCounter		= 0;
+    smoothedVol     = 0.0;
+    scaledVol		= 0.0;
+    
+    soundStream.setup(this, 0, 2, 44100, bufferSize, 4);
+    
+    //**********************************************
+    //**********************************************
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+    //don't move the points if we are using the camera
+    if(!usecamera){
+        ofVec3f sumOfAllPoints(0,0,0);
+        for(unsigned int i = 0; i < points.size(); i++){
+            points[i].z -= 4;
+            sumOfAllPoints += points[i];
+        }
+        center = sumOfAllPoints / points.size();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackgroundGradient(ofColor(60,60,60), ofColor(10,10,10));
+
+    
+    //if we're using the camera, start it.
+    //everything that you draw between begin()/end() shows up from the view of the camera
+    if(usecamera){
+        camera.begin();
+    }
+    
+    
+    
+    
+    
+    //do the same thing from the first example...
+    ofMesh mesh;
+    
+    
+//first*************************************************************
+//******************************************************************
+    
+    ofPushMatrix();
+    mesh.setMode(OF_PRIMITIVE_POINTS);
+//    ofSetColor(255);
+
+
+    for(unsigned int i = 1; i < points.size(); i++){
+        //find this point and the next point
+        ofVec3f thisPoint = points[i-1];
+        ofVec3f nextPoint = points[i];
+        
+        //get the direction from one to the next.
+        //the ribbon should fan out from this direction
+        ofVec3f direction = (nextPoint - thisPoint);
+        
+        //get the distance from one point to the next
+        float distance = direction.length();
+        
+        //get the normalized direction. normalized vectors always have a length of one
+        //and are really useful for representing directions as opposed to something with length
+        ofVec3f unitDirection = direction.normalized();
+        
+        //find both directions to the left and to the right
+        ofVec3f toTheLeft = unitDirection.getRotated(-90, ofVec3f(0,0,1));
+        ofVec3f toTheRight = unitDirection.getRotated(90, ofVec3f(0,0,1));
+        
+        //use the map function to determine the distance.
+        //the longer the distance, the narrower the line.
+        //this makes it look a bit like brush strokes
+        float thickness = ofMap(distance, 0, 60, 20, 2, true);
+        
+        //calculate the points to the left and to the right
+        //by extending the current point in the direction of left/right by the length
+        ofVec3f leftPoint = thisPoint+toTheLeft*thickness;
+        ofVec3f rightPoint = thisPoint+toTheRight*thickness;
+        
+        //add these points to the triangle strip
+        mesh.addVertex(ofVec3f(leftPoint.x, leftPoint.y, leftPoint.z));
+        mesh.addVertex(ofVec3f(rightPoint.x, rightPoint.y, rightPoint.z));
+    }
+    
+    for(unsigned int i = 1; i < points.size(); i++){
+        
+        //find this point and the next point
+        ofVec3f thisPoint = points[i-1];
+        ofVec3f nextPoint = points[i];
+        
+        //get the direction from one to the next.
+        //the ribbon should fan out from this direction
+        ofVec3f direction = (nextPoint - thisPoint);
+        
+        //get the distance from one point to the next
+        float distance = direction.length();
+        
+        //get the normalized direction. normalized vectors always have a length of one
+        //and are really useful for representing directions as opposed to something with length
+        ofVec3f unitDirection = direction.normalized();
+        
+        //find both directions to the left and to the right
+        ofVec3f toTheLeft = unitDirection.getRotated(-90, ofVec3f(0,0,1));
+        ofVec3f toTheRight = unitDirection.getRotated(90, ofVec3f(0,0,1));
+        
+        //use the map function to determine the distance.
+        //the longer the distance, the narrower the line.
+        //this makes it look a bit like brush strokes
+        float thickness = ofMap(distance, 0, 60, 20, 2, true)-10;
+        
+        //calculate the points to the left and to the right
+        //by extending the current point in the direction of left/right by the length
+        ofVec3f leftPoint = thisPoint+toTheLeft*thickness;
+        ofVec3f rightPoint = thisPoint+toTheRight*thickness;
+        
+        //add these points to the triangle strip
+        mesh.addVertex(ofVec3f(leftPoint.x, leftPoint.y, leftPoint.z));
+        mesh.addVertex(ofVec3f(rightPoint.x, rightPoint.y, rightPoint.z));
+    }
+    
+    
+    for(unsigned int i = 1; i < points.size(); i++){
+        
+        //find this point and the next point
+        ofVec3f thisPoint = points[i-1];
+        ofVec3f nextPoint = points[i];
+        
+        //get the direction from one to the next.
+        //the ribbon should fan out from this direction
+        ofVec3f direction = (nextPoint - thisPoint);
+        
+        //get the distance from one point to the next
+        float distance = direction.length();
+        
+        //get the normalized direction. normalized vectors always have a length of one
+        //and are really useful for representing directions as opposed to something with length
+        ofVec3f unitDirection = direction.normalized();
+        
+        //find both directions to the left and to the right
+        ofVec3f toTheLeft = unitDirection.getRotated(-90, ofVec3f(0,0,1));
+        ofVec3f toTheRight = unitDirection.getRotated(90, ofVec3f(0,0,1));
+        
+        //use the map function to determine the distance.
+        //the longer the distance, the narrower the line.
+        //this makes it look a bit like brush strokes
+        float thickness = ofMap(distance, 0, 60, 20, 2, true)-20;
+        
+        //calculate the points to the left and to the right
+        //by extending the current point in the direction of left/right by the length
+        ofVec3f leftPoint = thisPoint+toTheLeft*thickness;
+        ofVec3f rightPoint = thisPoint+toTheRight*thickness;
+        
+        //add these points to the triangle strip
+        mesh.addVertex(ofVec3f(leftPoint.x, leftPoint.y, leftPoint.z));
+        mesh.addVertex(ofVec3f(rightPoint.x, rightPoint.y, rightPoint.z));
+    }
+    
+
+
+    
+    mesh.draw();
+    ofPopMatrix();
+    
+//*************************************************************
+//******************************************************************
+    
+    ofPushMatrix();
+    mesh.setMode(OF_PRIMITIVE_POINTS);
+    ofSetColor(255,255,0);
+
+
+    for(unsigned int i = 1; i < points.size(); i++){
+        
+        //find this point and the next point
+        ofVec3f thisPoint = points[i-1];
+        ofVec3f nextPoint = points[i];
+        
+        //get the direction from one to the next.
+        //the ribbon should fan out from this direction
+        ofVec3f direction = (nextPoint - thisPoint);
+        
+        //get the distance from one point to the next
+        float distance = direction.length();
+        
+        //get the normalized direction. normalized vectors always have a length of one
+        //and are really useful for representing directions as opposed to something with length
+        ofVec3f unitDirection = direction.normalized();
+        
+        //find both directions to the left and to the right
+        ofVec3f toTheLeft = unitDirection.getRotated(-90, ofVec3f(0,0,1));
+        ofVec3f toTheRight = unitDirection.getRotated(90, ofVec3f(0,0,1));
+        
+        //use the map function to determine the distance.
+        //the longer the distance, the narrower the line.
+        //this makes it look a bit like brush strokes
+        float thickness = ofMap(distance, 0, 60, 20+smoothedVol*1400, 2, true) - 15;
+        
+        //calculate the points to the left and to the right
+        //by extending the current point in the direction of left/right by the length
+        ofVec3f leftPoint = thisPoint+toTheLeft*thickness;
+        ofVec3f rightPoint = thisPoint+toTheRight*thickness;
+        
+        //add these points to the triangle strip
+        mesh.addVertex(ofVec3f(leftPoint.x-5, leftPoint.y-5, leftPoint.z-10));
+        mesh.addVertex(ofVec3f(rightPoint.x-5, rightPoint.y-5, rightPoint.z-10));
+    }
+    
+    
+    for(unsigned int i = 1; i < points.size(); i++){
+        
+        //find this point and the next point
+        ofVec3f thisPoint = points[i-1];
+        ofVec3f nextPoint = points[i];
+        
+        //get the direction from one to the next.
+        //the ribbon should fan out from this direction
+        ofVec3f direction = (nextPoint - thisPoint);
+        
+        //get the distance from one point to the next
+        float distance = direction.length();
+        
+        //get the normalized direction. normalized vectors always have a length of one
+        //and are really useful for representing directions as opposed to something with length
+        ofVec3f unitDirection = direction.normalized();
+        
+        //find both directions to the left and to the right
+        ofVec3f toTheLeft = unitDirection.getRotated(-90, ofVec3f(0,0,1));
+        ofVec3f toTheRight = unitDirection.getRotated(90, ofVec3f(0,0,1));
+        
+        //use the map function to determine the distance.
+        //the longer the distance, the narrower the line.
+        //this makes it look a bit like brush strokes
+        float thickness = ofMap(distance, 0, 60, 20+smoothedVol*1000, 2, true) - 20;
+        
+        //calculate the points to the left and to the right
+        //by extending the current point in the direction of left/right by the length
+        ofVec3f leftPoint = thisPoint+toTheLeft*thickness;
+        ofVec3f rightPoint = thisPoint+toTheRight*thickness;
+        
+        //add these points to the triangle strip
+        mesh.addVertex(ofVec3f(leftPoint.x-8, leftPoint.y-8, leftPoint.z-20));
+        mesh.addVertex(ofVec3f(rightPoint.x-8, rightPoint.y-8, rightPoint.z-20));
+    }
+    
+    
+    for(unsigned int i = 1; i < points.size(); i++){
+        
+        //find this point and the next point
+        ofVec3f thisPoint = points[i-1];
+        ofVec3f nextPoint = points[i];
+        
+        //get the direction from one to the next.
+        //the ribbon should fan out from this direction
+        ofVec3f direction = (nextPoint - thisPoint);
+        
+        //get the distance from one point to the next
+        float distance = direction.length();
+        
+        //get the normalized direction. normalized vectors always have a length of one
+        //and are really useful for representing directions as opposed to something with length
+        ofVec3f unitDirection = direction.normalized();
+        
+        //find both directions to the left and to the right
+        ofVec3f toTheLeft = unitDirection.getRotated(-90, ofVec3f(0,0,1));
+        ofVec3f toTheRight = unitDirection.getRotated(90, ofVec3f(0,0,1));
+        
+        //use the map function to determine the distance.
+        //the longer the distance, the narrower the line.
+        //this makes it look a bit like brush strokes
+        float thickness = ofMap(distance, 0, 60, 20+smoothedVol*700, 2, true) - 25;
+        
+        //calculate the points to the left and to the right
+        //by extending the current point in the direction of left/right by the length
+        ofVec3f leftPoint = thisPoint+toTheLeft*thickness;
+        ofVec3f rightPoint = thisPoint+toTheRight*thickness;
+        
+        //add these points to the triangle strip
+        mesh.addVertex(ofVec3f(leftPoint.x-10, leftPoint.y-10, leftPoint.z-30));
+        mesh.addVertex(ofVec3f(rightPoint.x-10, rightPoint.y-10, rightPoint.z-30));
+    }
+    
+    
+    for(unsigned int i = 1; i < points.size(); i++){
+        
+        //find this point and the next point
+        ofVec3f thisPoint = points[i-1];
+        ofVec3f nextPoint = points[i];
+        
+        //get the direction from one to the next.
+        //the ribbon should fan out from this direction
+        ofVec3f direction = (nextPoint - thisPoint);
+        
+        //get the distance from one point to the next
+        float distance = direction.length();
+        
+        //get the normalized direction. normalized vectors always have a length of one
+        //and are really useful for representing directions as opposed to something with length
+        ofVec3f unitDirection = direction.normalized();
+        
+        //find both directions to the left and to the right
+        ofVec3f toTheLeft = unitDirection.getRotated(-90, ofVec3f(0,0,1));
+        ofVec3f toTheRight = unitDirection.getRotated(90, ofVec3f(0,0,1));
+        
+        //use the map function to determine the distance.
+        //the longer the distance, the narrower the line.
+        //this makes it look a bit like brush strokes
+        float thickness = ofMap(distance, 0, 60, 20+smoothedVol*700, 2, true) - 30;
+        
+        //calculate the points to the left and to the right
+        //by extending the current point in the direction of left/right by the length
+        ofVec3f leftPoint = thisPoint+toTheLeft*thickness;
+        ofVec3f rightPoint = thisPoint+toTheRight*thickness;
+        
+        //add these points to the triangle strip
+        mesh.addVertex(ofVec3f(leftPoint.x-13, leftPoint.y-13, leftPoint.z-30));
+        mesh.addVertex(ofVec3f(rightPoint.x-13, rightPoint.y-13, rightPoint.z-30));
+    }
+    
+    
+    mesh.draw();
+    ofPopMatrix();
+
+//*************************************************************
+//******************************************************************
+    
+    
+
+    
+
+    
+    
+    
+    
+    //end the shape
+    
+    
+    
+    //if we're using the camera, take it away
+    if(usecamera){
+        camera.end();
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    //hitting any key swaps the camera view
+    usecamera = !usecamera;
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    //if we are using the camera, the mouse moving should rotate it around the whole sculpture
+    if(usecamera){
+        float rotateAmount = ofMap(ofGetMouseX(), 0, ofGetWidth(), 0, 360);
+        ofVec3f furthestPoint;
+        if (points.size() > 0) {
+            furthestPoint = points[0];
+        }
+        else
+        {
+            furthestPoint = ofVec3f(x, y, 0);
+        }
+        
+        ofVec3f directionToFurthestPoint = (furthestPoint - center);
+        ofVec3f directionToFurthestPointRotated = directionToFurthestPoint.rotated(rotateAmount, ofVec3f(0,1,0));
+        camera.setPosition(center + directionToFurthestPointRotated);
+        camera.lookAt(center);
+    }
+    //otherwise add points like before
+    else{
+        ofVec3f mousePoint(x,y,0);
+        points.push_back(mousePoint);
+    }
+}
+
+
+//**********************************************
+//**********************************************
+//audio input class
+void ofApp::audioIn(float * input, int bufferSize, int nChannels){
+    
+    float curVol = 0.0;
+    
+    // samples are "interleaved"
+    int numCounted = 0;
+    
+    //lets go through each sample and calculate the root mean square which is a rough way to calculate volume
+    for (int i = 0; i < bufferSize; i++){
+        left[i]		= input[i*2]*0.5;
+        //        right[i]	= input[i*2+1]*0.5;
+        
+        curVol += left[i] * left[i];
+        //        curVol += right[i] * right[i];
+        numCounted+=2;
+    }
+    
+    //this is how we get the mean of rms :)
+    curVol /= (float)numCounted;
+    
+    // this is how we get the root of rms :)
+    curVol = sqrt( curVol );
+    
+    smoothedVol *= 0.93;
+    smoothedVol += 0.07 * curVol;
+    
+    bufferCounter++;
+    
+}
+//**********************************************
+//**********************************************
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
